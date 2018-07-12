a2980a0844ddccab6f40ef4742ae2594f2cc60eb
Kaori.Nishikawa <nishikawa@ponos.co.jp>
2018-07-12 12:22:30 +0900

    initial commit

---
+++ src/org/pankuzu/java/sample/Test.java
@@ -0,0 +1,11 @@
+package org.pankuzu.java.sample;
+
+public class Test {
+    public static void main(String[] args) {
+        sayHello();
+    }
+
+    private static void sayHello() {
+        System.out.println("Hello JAVA");
+    }
+}
